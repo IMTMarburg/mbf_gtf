118170ac9e6d3ba2ec341f6300bba4968e8142a4
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2022-04-05 16:05:22 +0200

    flake imports numpy & pandas for testing

--- flake.nix
+++ flake.nix
@@ -17,13 +17,25 @@
           buildInputs = [
             rust-bin.stable."1.59.0".default
             #(pkgs.python39.withPackages (pp: [ pp.maturin ]))
-            (pkgs.python39.withPackages (pp: [ pp.cffi ]))
             pkgs.maturin
             bacon
             #pkgs.python36
+            pkgs.python3
+            pkgs.python3.pkgs.numpy
+            pkgs.python3.pkgs.pandas
+
           ];
 
-          shellHook = "";
+          shellHook = ''
+            # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
+            # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
+            export PIP_PREFIX=$(pwd)/_build/pip_packages
+            export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+            export PATH="$PIP_PREFIX/bin:$PATH"
+            unset SOURCE_DATE_EPOCH
+          '';
+
         };
+
       });
 }
